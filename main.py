f2f8a898f4c115712c3da3b9ecf0c6ac4dd72b3e
Mohamed Ibrahim Hany <60690890+mihany@users.noreply.github.com>
2023-03-11 15:20:46 +0200

    some code added

---
+++ main.py
@@ -0,0 +1,13 @@
+
+message = "Nope"
+
+# control statement => the statments are executed one time onlyyyy
+
+if message == "No":
+    print("She said No!")
+elif message == "Yes":
+    print("Lucky Me")
+else:
+    print("Error")
+
+print(type(message))
